d442bb339f5fddb7a6d3f63b3e8b9134ae60aa5b
Levin-Alexey <levin.penza@yandex.ru>
2025-11-17 11:34:27 +0300

    fix llm

--- llm_client.py
+++ llm_client.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
-MODEL = "deepseek/deepseek-chat-v3.1:free"
+MODEL = "google/gemini-2.5-flash"
 REFERER = "https://maldina.ru"  # Можно заменить на свой сайт
 TITLE = "MaldinaBot"
 
